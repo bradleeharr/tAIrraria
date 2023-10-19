66e982bd3f72046079bb8ee9142c2cfd6c33a3da
Bradlee Harrison <bradlee.harrison22@gmail.com>
2023-10-18 21:21:17 -0500

    Chat loop and receive server

--- TerrAIPy/main.py
+++ TerrAIPy/main.py
@@ -1,4 +1,5 @@
 import socket
+from runchat import *
 
 
 def start_server():
@@ -10,26 +11,34 @@ def start_server():
     server_socket.listen(1)
 
     print(f"Server started at {host}:{port}")
+    print("Waiting for a connection...")
+    client_socket, addr = server_socket.accept()
+    print(f"Connection from {addr}")
+    return server_socket, client_socket
 
-    while True:
-        print("Waiting for a connection...")
-        client_socket, addr = server_socket.accept()
-        print(f"Connection from {addr}")
 
-        data = client_socket.recv(1024)
-        if not data:
-            print("No data received, closing connection")
-            client_socket.close()
-            continue
-
-        print(f"Received data: {data.decode('utf-8')}")
-
-        # Sending a response back to the client
-        response = "Command Received"
-        client_socket.send(response.encode('utf-8'))
-
+def receive_server(client_socket):
+    data = client_socket.recv(1024)
+    if not data:
+        print("No data received, closing connection")
         client_socket.close()
+    else:
+        print(f"Received data: {data.decode('utf-8')}")
+        # Sending a response back to the client
+        response = "Data Received"
+        client_socket.send(response.encode('utf-8'))
+    return client_socket, data;
+
+
+def close_server(client_socket):
+    client_socket.close()
 
 
 if __name__ == "__main__":
-    start_server()
+    messages = get_messages_and_api_key();
+    server, client = start_server()
+    while True:
+        data = receive_server(client)
+        chat_once(messages, data)
+
+    close_server(client)
